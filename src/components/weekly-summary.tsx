281d38b089af7ce2edb9b9ef8a1e04ad24a9d667
matheustorresdev97 <matheustorresdev@gmail.com>
2024-09-17 17:46:39 -0300

    dividindo components

--- src/components/weekly-summary.tsx
+++ src/components/weekly-summary.tsx
@@ -1,62 +1,64 @@
-import { DialogTrigger } from "@radix-ui/react-dialog";
-import { InOrbitIcon } from "./in-orbit-icon";
 import { CheckCircle2, Plus } from "lucide-react";
-import { Progress, ProgressIndicator } from "@radix-ui/react-progress";
+import { InOrbitIcon } from "./in-orbit-icon";
+import { DialogTrigger } from "@radix-ui/react-dialog";
+import { Button } from "./ui/button";
+import { OutlineButton } from "./ui/outline-button";
+import { Progress, ProgressIndicator } from "./ui/progress-bar";
+import { Separator } from "./ui/separator";
 
 export function WeeklySummary() {
-    return (
-        <main className="max-w-[480px] py-10 px-5 mx-auto flex flex-col gap-6">
-            <div className="flex items-center justify-between">
-                <div className="flex items-center gap-3">
-                    <InOrbitIcon />
-                    <span className="text-lg font-semibold">05 a 12 de Agosto</span>
-                </div>
+  return (
+    <main className="max-w-[480px] py-10 px-5 mx-auto flex flex-col gap-6">
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-3">
+          <InOrbitIcon />
+          <span className="text-lg font-semibold">05 a 12 de Agosto</span>
+        </div>
 
-                <DialogTrigger className="flex items-center gap-2 px-3 py-1.5 rounded-lg bg-violet-500 text-indigo-50 text-sm font-medium tracking-tight hover:bg-violet-600">
-                    <Plus className="size-4" />
-                    Cadastrar meta
-                </DialogTrigger>
-            </div>
+        <DialogTrigger asChild>
+          <Button size="sm">
+            <Plus className="size-4" />
+            Cadastrar meta
+          </Button>
+        </DialogTrigger>
+      </div>
 
-            <div className="flex flex-col gap-3">
-                <Progress value={50} max={100} className="bg-zinc-900 rounded-full h-2">
-                    <ProgressIndicator className="bg-gradient-to-r from-pink-500 to-violet-500 w-1/2 h-2 rounded-full" />
-                </Progress>
+      <div className="flex flex-col gap-3">
+        <Progress value={50} max={100}>
+          <ProgressIndicator style={{ width: '50%' }} />
+        </Progress>
 
-                <div className="flex items-center justify-between text-xs text-zinc-400">
-                    <span>Você completou <span className="text-zinc-100">8</span> de <span className="text-zinc-100">15</span> metas nessa semana.</span>
-                    <span>58%</span>
-                </div>
-            </div>
+        <div className="flex items-center justify-between text-xs text-zinc-400">
+          <span>Você completou <span className="text-zinc-100">8</span> de <span className="text-zinc-100">15</span> metas nessa semana.</span>
+          <span>58%</span>
+        </div>
+      </div>
 
-            <div className="h-px bg-zinc-900" />
+      <Separator />
+      
+      <div className="flex flex-wrap gap-3">
+        <OutlineButton>
+          <Plus className="size-4 text-zinc-600" />
+          Meditar
+        </OutlineButton>
 
-            <div className="flex flex-wrap gap-3">
-                <button className="flex items-center px-3 py-2 gap-2 leading-none rounded-full border border-dashed border-zinc-800 text-sm text-zinc-300 hover:border-zinc-700">
-                    <Plus className="size-4 text-zinc-600" />
-                    Meditar
-                </button>
+        <OutlineButton>
+          <Plus className="size-4 text-zinc-600" />
+          Praticar exercício
+        </OutlineButton>
 
-                <button className="flex items-center px-3 py-2 gap-2 leading-none rounded-full border border-dashed border-zinc-800 text-sm text-zinc-300 hover:border-zinc-700">
-                    <Plus className="size-4 text-zinc-600" />
-                    Praticar exercício
-                </button>
+        <OutlineButton>
+          <Plus className="size-4 text-zinc-600" />
+          Levantar cedo
+        </OutlineButton>
 
-                <button className="flex items-center px-3 py-2 gap-2 leading-none rounded-full border border-dashed border-zinc-800 text-sm text-zinc-300 hover:border-zinc-700">
-                    <Plus className="size-4 text-zinc-600" />
-                    Levantar cedo
-                </button>
+        <OutlineButton>
+          <Plus className="size-4 text-zinc-600" />
+          Manter uma boa alimentação
+        </OutlineButton>
+      </div>
 
-                <button
-                    disabled
-                    className="flex items-center px-3 py-2 gap-2 leading-none rounded-full border border-dashed border-zinc-800 text-sm text-zinc-300 hover:border-zinc-700 disabled:opacity-50 disabled:pointer-events-none"
-                >
-                    <Plus className="size-4 text-zinc-600" />
-                    Manter uma boa alimentação
-                </button>
-            </div>
-
-            <div className="space-y-6">
+      <div className="space-y-6">
         <h2 className="text-xl font-medium">Sua semana</h2>
         <div className="space-y-4">
           <h3 className="font-medium">Hoje <span className="text-zinc-400 text-xs">(06 de agosto)</span></h3>
@@ -126,6 +128,6 @@ export function WeeklySummary() {
           </ul>
         </div>
       </div>
-        </main>
-    );
-};
+    </main>
+  )
+}
