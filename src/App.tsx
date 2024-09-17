f3c35058cb6a9dd52aa25a55c3015a96ddb0a421
matheustorresdev97 <matheustorresdev@gmail.com>
2024-09-17 18:07:36 -0300

    integração com api

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,30 @@
 import { Dialog } from "@radix-ui/react-dialog";
 import { WeeklySummary } from "./components/weekly-summary";
 import { CreateGoal } from "./components/create-goal";
+import { EmptyGoals } from "./components/empty-goals";
+import { useQuery } from "@tanstack/react-query";
+import { getSummary } from "./http/get-summary";
+import { Loader2 } from "lucide-react";
 
 export default function App() {
-  return (
-   <Dialog>
-    {/* <EmptyGoals /> */}
-    <WeeklySummary />
+  const { data, isLoading } = useQuery({
+    queryKey: ['summary'],
+    queryFn: getSummary,
+  })
 
-    <CreateGoal />
-   </Dialog>
+  if (isLoading || !data) {
+    return (
+      <div className="h-screen flex items-center justify-center">
+        <Loader2 className="text-zinc-500 animate-spin size-10" />
+      </div>
+    )
+  }
+
+  return (
+    <Dialog>
+      {data.summary.total > 0 ? <WeeklySummary summary={data.summary} /> : <EmptyGoals />}
+
+      <CreateGoal />
+    </Dialog>
   )
 }
